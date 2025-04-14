145ee4566647fad5dfcaaa72de2beabfaf8055c3
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-04-15 00:05:47 +0900

    あげ忘れてた

--- frontend/nextjs/next.config.ts
+++ frontend/nextjs/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "login-form-test"; // 例: next-login-app
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: isGitHubPages ? `/${repoName}` : "",
+  assetPrefix: isGitHubPages ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
