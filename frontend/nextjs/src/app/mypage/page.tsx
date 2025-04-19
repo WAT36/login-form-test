220ae5c50324f841a57cb5ad52131de720846fb3
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-04-20 01:31:34 +0900

    認証用コンポーネント作成

--- frontend/nextjs/src/app/mypage/page.tsx
+++ frontend/nextjs/src/app/mypage/page.tsx
@@ -1,30 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import RequireAuth from "../RequireAuth";
 
 export default function MyPage() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-  const [, setToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    const idToken = localStorage.getItem("idToken");
-
-    if (!idToken) {
-      router.replace("/login"); // 未ログインならログインページへ
-    } else {
-      setToken(idToken);
-      setLoading(false);
-    }
-  }, []);
-
-  if (loading) return <p>読み込み中...</p>;
-
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>マイページ</h1>
-      <p>ようこそ、ログイン済みのユーザーさん！</p>
-    </div>
+    <RequireAuth>
+      <div style={{ padding: "2rem" }}>
+        <h1>マイページ</h1>
+        <p>ようこそ、ログイン済みのユーザーさん！</p>
+      </div>
+    </RequireAuth>
   );
 }
